ef504f429334218f2eb492ff7d9d3a8fbcf1518e
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-06-04 03:40:02 +0300

    הגדרת מסך פתיחה, ונתוני אפליקצייה
    הנתונים כוללים שם תצוגה, מספר גרסה וכו'

--- src/windows/runner/main.cpp
+++ src/windows/runner/main.cpp
@@ -7,14 +7,9 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
-
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
@@ -27,7 +22,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"shamor_vezachor", origin, size)) {
+  if (!window.Create(L"שמור וזכור", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
